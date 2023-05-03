374c76466cb80ac2bfd5c24a300a858f998a5220
hamaup <32893785+hamaup@users.noreply.github.com>
2023-05-03 15:32:26 +0900

    add tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { GTMScript, GTMNoScript } from '../components/Gtm';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <GTMScript />
+      </Head>
       <body>
+        <GTMNoScript />
         <Main />
         <NextScript />
       </body>
